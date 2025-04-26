210d008476359ee955ad10293ae3847e1fa2ab87
labeebbappu <labeeb.in@outlook.com>
2025-04-26 14:31:47 +0300

    feat: add about and contact pages with basic layout
    Introduce new pages for the about and contact sections of the application. Each page includes a consistent layout with a top menu and footer bar, ensuring a unified user experience.

---
+++ app/about/page.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import TopMenu from "@/components/home/TopMenu";
+import FootBar from "@/components/home/FootBar"; 
+
+const AboutPage = async () => {
+  return (
+    <div className="min-h-screen">
+      <TopMenu />
+      
+
+      <div className="min-h-screen">
+      about primo
+      </div>
+
+      <FootBar />
+      
+    </div>
+  );
+};
+
+export default AboutPage;
