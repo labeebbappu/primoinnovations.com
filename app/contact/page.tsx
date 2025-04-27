e4f617ad5c9f5189fa7d716d9fa0597d052c8138
labeebbappu <labeeb.in@outlook.com>
2025-04-27 20:26:49 +0300

    feat(contact): add Hero component and integrate into contact page
    Introduce a new Hero component for the contact page to enhance user interaction and provide a clear call-to-action for WhatsApp communication. Additionally, update the PrimoApps component to include features not included in each plan and replace buttons with links for better user engagement.

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import TopMenu from "@/components/home/TopMenu";
-import FootBar from "@/components/home/FootBar"; 
+import FootBar from "@/components/home/FootBar";
+import Hero from "@/components/contact/Hero";
 
 const ContactPage = async () => {
   return (
     <div className="min-h-screen">
       <TopMenu />
+
+      <Hero
+        title="We are a whatsapp message away."
+        subtitle="ensuring seamless, direct, and friendly interaction. We believe in straightforward, personalized communication tailored to your unique needs."
+      />
       
-      <div className="min-h-screen">
-      contact  primo
-      </div>
       <FootBar />
-      
     </div>
   );
 };
